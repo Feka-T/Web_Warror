fd1fe074cb00a0ab63105db51e708a92afdad3e6
Fekadu Tadesse <aaumsccs@gmail.com>
2023-11-22 14:35:43 +0300

    Migrate to this version and test, gatsby

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,31 +1,39 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+// import { Link } from "gatsby";
+import Layout from "./components/Layout";
 
-const pageStyles = {
+/* const pageStyles = {
   color: "#232129",
   padding: "96px",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
-}
+};
 
 const paragraphStyles = {
   marginBottom: 48,
-}
+};
 const codeStyles = {
   color: "#8A6534",
   padding: 4,
   backgroundColor: "#FFF4DB",
   fontSize: "1.25rem",
   borderRadius: 4,
-}
+}; */
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
+    <Layout>
+      <div>
+        <h2>404</h2>
+        <p>Sorry, that page doesn't exist (yet)!</p>
+      </div>
+    </Layout>
+
+    /*     <main style={pageStyles}>
       <h1 style={headingStyles}>Page not found</h1>
       <p style={paragraphStyles}>
         Sorry 😔, we couldn’t find what you were looking for.
@@ -40,10 +48,10 @@ const NotFoundPage = () => {
         <br />
         <Link to="/">Go home</Link>.
       </p>
-    </main>
-  )
-}
+    </main> */
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head = () => <title>Not found</title>
+export const Head = () => <title>Not found</title>;
