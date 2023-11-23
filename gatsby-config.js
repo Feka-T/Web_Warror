0e9b62008aebc4f630b92160d72baa896d40b1a8
Fekadu Tadesse <aaumsccs@gmail.com>
2023-11-23 16:36:08 +0300

    styling - global and module graphql - page and static static image file load

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,9 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [],
-}
+  siteMetadata: {
+    title: "Web Warrior",
+    description: "web dev portfolio",
+    copyright: "This website is copyright 2021 Web Warrior",
+  },
+};
