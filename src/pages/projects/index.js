e35027b2e556ab4e9091e5fdaa63f768a4a69d51
Fekadu Tadesse <aaumsccs@gmail.com>
2023-11-27 16:26:14 +0300

    fetching and sorting

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,13 +1,27 @@
 import React from "react";
 import Layout from "../../components/Layout";
 import * as styles from "../../styles/projects.module.css";
+import { Link, graphql } from "gatsby";
 
-const Projects = () => {
+const Projects = ({ data }) => {
+  console.log(data);
+  const projects = data.allMarkdownRemark.nodes;
+
   return (
     <Layout>
       <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
+        <div className={styles.projects}>
+          {projects.map((project) => (
+            <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   );
@@ -16,3 +30,19 @@ const Projects = () => {
 export default Projects;
 
 export const Head = () => <title>Portfolio Page | Fekadu Gatsby</title>;
+
+// export page query
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`;
